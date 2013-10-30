1ab251e4c206ee72a4f3f4e84f05101d916bd72a
Johaned <johan.tique@codescrum.com>
2013-10-29 22:20:24 -0500

    remove id field in decision model

--- src/edu/unicauca/slowchart/persistence/model/Decision.java
+++ src/edu/unicauca/slowchart/persistence/model/Decision.java
@@ -12,17 +12,15 @@ import java.util.List;
  */
 public class Decision {
 	private String name;
-	private Long id;
 	private String process;
 	
 	public Decision() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Decision(String name, Long id, String process) {
+	public Decision(String name, String process) {
 		super();
 		this.name = name;
-		this.id = id;
 		this.process = process;
 	}
 	public String getName() {
@@ -30,12 +28,6 @@ public class Decision {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
 	}
 	public String getProcess() {
 		return process;
