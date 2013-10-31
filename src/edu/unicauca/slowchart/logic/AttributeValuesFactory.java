ead1f968b943fc73f1ebae1bfe0ac9d952dd2b42
Johaned <johan.tique@codescrum.com>
2013-10-30 22:08:16 -0500

    add HashMap with fictitious attributes values

---
+++ src/edu/unicauca/slowchart/logic/AttributeValuesFactory.java
@@ -0,0 +1,16 @@
+package edu.unicauca.slowchart.logic;
+
+import java.util.HashMap;
+
+public class AttributeValuesFactory {
+	public static HashMap<String, String> create_fictitious_attr_values(){
+		HashMap<String, String> attributes = new HashMap<String, String>();
+		attributes.put("1e456ef8456a46b6c1", "38");
+		attributes.put("1e456ef8456a46b6c2", "5");
+		attributes.put("1e456ef8456a46b6c3", "2");
+		attributes.put("1e456ef8456a46b6c4", "4");
+		attributes.put("1e456ef8456a46b6c5", "3");
+		attributes.put("1e456ef8456a46b6c6", "MPEG4");
+		return attributes;
+	}
+}
